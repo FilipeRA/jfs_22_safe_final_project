807424682ded72ee3c9ee9b4150352aeede5364f
Shalihin Ahmad <shalihinsgp@gmail.com>
2022-11-27 03:35:08 +0100

    fixed

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -3,15 +3,25 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 const Footer = () => (
-  <div className="footer-container">
-    <footer className={styles.footer}>
-      Copyright 2022 SAFE
-      <Link href="https://github.com/FilipeRA/jfs_22_safe_final_project">
-        Github
-      </Link>
-      Salt JFS Fall 2022
-    </footer>
-  </div>
+  <footer className="footer-container">
+
+    <span className="footer--links-left">
+      © Copyright 2022 SAFE. All Rights Reserved.
+    </span>
+
+    <ul className="footer--links-right">
+      <li>
+        {' '}
+        <Link href="https://github.com/FilipeRA/jfs_22_safe_final_project">
+          Github
+        </Link>
+      </li>
+      <li>
+        Salt JFS Fall 2022
+      </li>
+    </ul>
+
+  </footer>
 
 );
 
