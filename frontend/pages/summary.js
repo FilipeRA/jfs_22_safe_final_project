e405d0b67de415ac17c28bfdfbb0ccdb5e121057
Shalihin Ahmad <shalihinsgp@gmail.com>
2022-11-28 23:48:57 +0100

    summary page done

--- frontend/pages/summary.js
+++ frontend/pages/summary.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Head from 'next/head';
 import { signIn, useSession, signOut } from 'next-auth/react';
 import {
-  Navbar, Dropdown, Avatar,
+  Navbar, Dropdown, Avatar, Card, Button,
 } from 'flowbite-react';
 import styles from '../styles/Summary.module.css';
 import navStyles from '../styles/Navbar.module.css';
@@ -27,6 +27,8 @@ const Summary = () => {
     }
   }, [session]);
 
+  console.log(lastUserHistory.professionalId);
+
   if (status === 'authenticated') {
     return (
       <div>
@@ -36,16 +38,58 @@ const Summary = () => {
           <link rel="icon" href="/HireMeHead.png" />
         </Head>
         <main className={styles.summary}>
-          <div className={styles.invoiceContainer}>
-            <h2>
-              Professional:
-              {' '}
-              {lastUserHistory.professionalName}
-            </h2>
-          </div>
-          <Link href="/">
-            <p>Back to Homepage</p>
-          </Link>
+          <main className={styles.invoiceContainer}>
+            <Card imgSrc="https://images.unsplash.com/photo-1596583164564-5d6e7a9e0c68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Congratulations! You have successfully hired
+                {' '}
+                <Link href={`/gallery/${lastUserHistory.professionalId}`} className={styles.proName}>
+                  {lastUserHistory.professionalName}
+                </Link>
+                .
+              </h5>
+              <div className="font-normal text-gray-700 dark:text-gray-400">
+                <p>
+                  {' '}
+                  The team at HireMe would like to thank you for using our services.
+                  We believe that you will greatly benefit from this.
+                </p>
+                <br />
+                <p>
+                  {' '}
+                  If you wish to look at our other services, please
+                  {' '}
+                  <Link href="/gallery?location=stockholm&service=" className={styles.clickHere}> click here</Link>
+                  .
+                  {' '}
+                  Else you can browse our
+                  {' '}
+                  <Link href="/" className={styles.clickHere}> homepage</Link>
+                  .
+                </p>
+                <br />
+                <p>
+                  We look forward to receiving feedbacks from you!
+                </p>
+
+              </div>
+              <Button href="/account">
+                Go to my account
+                <svg
+                  className="ml-2 -mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd" />
+                </svg>
+              </Button>
+            </Card>
+
+          </main>
+
         </main>
         <nav className={navStyles.bottomNav}>
           <div className={navStyles.bottomNavLinks}>
