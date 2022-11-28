fa6ae9d23231503c9b98b458a7407b1fd35c8df9
Shalihin Ahmad <shalihinsgp@gmail.com>
2022-11-29 00:21:33 +0100

    fixed login and navbar

--- frontend/pages/summary.js
+++ frontend/pages/summary.js
@@ -39,7 +39,8 @@ const Summary = () => {
           <main className={styles.invoiceContainer}>
             <Card imgSrc="https://images.unsplash.com/photo-1596583164564-5d6e7a9e0c68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80">
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Congratulations! You have successfully hired
+                Congratulations!
+                You have successfully hired
                 {' '}
                 <Link href={`/gallery/${lastUserHistory.professionalId}`} className={styles.proName}>
                   {lastUserHistory.professionalName}
